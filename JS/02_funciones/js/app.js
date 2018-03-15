e390fa24ee19f138e6511944f888349bb448e152
senin8 <malizziaaries@gmail.com>
2018-03-15 13:17:19 +0100

    js4

--- JS/02_funciones/js/app.js
+++ JS/02_funciones/js/app.js
@@ -2,7 +2,7 @@
 //nombres en JS utilizan camelcase, empiezan con minusculas y si es palabra compuesta se pone mayuscula en la segunda, HolaMeLlamoSandra
 
 function saludar () {  //creo funcion, creo la invocación
-    console.log ("Hola amigo") //PRIMERO
+   console.log ("Hola amigo") //PRIMERO
 }
 
 //invoco funcion
---
+++ JS/02_funciones/js/multiplicacion.js
@@ -0,0 +1,9 @@
+
+function multiplicar ( a , b){ //PRIMERO, DECLARACIÓN DE LA FUNCIÓN
+    c = a * b
+    console.log ("El resultado de la multiplicación es ",  c) //ESTA ES OTRA FORMA DE CARGAR EL SCRIPT DESDE LA CONSOLA 
+    
+}
+
+ multiplicar (5 , 3) 
+  //ESTA ES OTRA FORMAR DE CARGAR EL SCRIPT DESDE EL PROPIO SCRIPT 
