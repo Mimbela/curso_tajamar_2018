5572e1214d013eb6beefc9d00c8307504140da85
senin8 <malizziaaries@gmail.com>
2018-03-26 23:54:35 +0200

    1

--- JS/02_funciones/js/constantes.js
+++ JS/02_funciones/js/constantes.js
@@ -39,9 +39,33 @@ moatrarCircunferencia(5)
 
 //el comportamiento de const y let es idéntico
 
-//******************************************* */
+//***************************FOR EACH**************** */
 let data= [{precio: 15}, {precio: 5} , {precio:8}]
 
 data.forEach (elen => console.log ("El resultado es ",  elen.precio*25 )
 )
 
+//**************************FOR EACH************************************************************* */
+//una viejecita vende 5 camisetas a distintos precios pero aplicandoles el mismo iva del 15%
+
+let camisetas= [{camiseta: 15}, {camiseta: 20}, {camiseta: 15}, {camiseta: 20}, {camiseta : 15}]
+camisetas.forEach(element => console.log ("El cliente debe pagar " , element.camiseta* 15)
+    
+);
+
+//**************************FOR EACH************************************************************* */
+//una viejecita vende 5 pantalone a distintos precios pero aplicandoles el mismo iva del 15%
+
+let pantalones= [{pantalon:23}, {pantalon: 34}, {pantalon : 45}, {pantalon : 98}, {pantalon : 54}]
+
+pantalones.forEach (totales => console.log ("La viejecita gana con la venta del pantalón " , totales.pantalon * 15))
+
+//******************************************aplicación del 20% IVA  a los ITEMS********************************************************************** */
+let productos = [{item : 20}, {item: 24} , {item :98}, {item : 42}]
+
+productos.forEach (total => console.log (" El total del producto es " , total.item * 20))
+
+//******************************************aplicación del 50% IVA  a los ITEMS de shampoes********************************************************************** */
+let shampoes= [{pantene: 12}, {pantene : 40}, {pantene : 29}]
+
+shampoes.forEach ( totality => console.log ("Lo que tiene que pagar por los shampoes es " , totality.pantene * 50 ))
